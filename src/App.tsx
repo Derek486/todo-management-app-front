86598a23776a006e0cf379dc5b2e808147d0124d
unknown <juanjosecondoripinto@gmail.com>
2025-03-27 19:08:24 -0500

    feat: tailwind | main

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen flex justify-center bg-ui-background">
+        <main className="p-4 text-center">
+          <header>
+            <h1 className="text-xl text-ui-main">Todo <span className="text-ui-paragraph">App</span></h1>
+            <p className="text-ui-paragraph">Create, read, update and delete all your tasks here</p>
+          </header>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
